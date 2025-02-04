103f0b643e0e56345440af3dab324d579094e513
Vishruth Bharath <vishruthbharath@gmail.com>
2025-02-04 10:25:10 -0800

    proj desc: v0.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='finiteautomata',
-    version='0.1.3',
+    version='0.1.5',
     description='A more intuitive way to create finite automata diagrams.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
